65b0fe49df64b521270101f5c0aef030e5f19095
Gandhy <gandhy.garcia@epn.edu.ec>
2022-08-30 20:14:14 -0500

    Add PreferenciasController

--- src/main/java/controlador/PreferenciasController.java
+++ src/main/java/controlador/PreferenciasController.java
@@ -8,8 +8,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import modelo.dao.DAOFactory;
+import modelo.entidades.Especie;
 import modelo.entidades.Mascota;
 import modelo.entidades.Preferencias;
+import modelo.entidades.Sexo;
 
 @WebServlet("/PreferenciasController")
 public class PreferenciasController extends HttpServlet {
@@ -20,6 +22,8 @@ public class PreferenciasController extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//int idMiMascota = Integer.parseInt(request.getParameter("idMiMascota"));
+		
 		int idMiMascota = 3;
 		Mascota miMascota = DAOFactory.getFactory().getMascotaDAO().getById(idMiMascota);
 		
@@ -28,6 +32,9 @@ public class PreferenciasController extends HttpServlet {
 		if (preferencias != null) {
 			request.setAttribute("preferenciasMiMascota", preferencias);
 		}
+		else {
+			request.setAttribute("preferenciasMiMascota", null);
+		}
 		
 		request.setAttribute("miMascota", miMascota);
 		request.getRequestDispatcher("/jsp/preferencias.jsp").forward(request, response);
@@ -35,10 +42,28 @@ public class PreferenciasController extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int idMiMascota = 3;
+		//int idMiMascota = Integer.parseInt(request.getParameter("idMiMascota"));
+		//String especie = Integer.parseInt(request.getParameter("especie"));
+		//String sexo = Integer.parseInt(request.getParameter("sexo"));
+		//int edadMinima = Integer.parseInt(request.getParameter("edadMinima"));
+		//int edadMaxima = Integer.parseInt(request.getParameter("edadMaxima"));
+		
+		//Mascota miMascota = DAOFactory.getFactory().getMascotaDAO().getById(idMiMascota);
+		
+		//Preferencias nuevasPreferenciasMiMascota = new Preferencias(miMascota, Especie.valueOf(especie), Sexo.valueOf(sexo), edadMinima, edadMaxima);
+		
+		
 		
 		Preferencias preferenciasMiMascota = DAOFactory.getFactory().getPreferenciasDAO().getPreferenciasByIdMascota(idMiMascota);
 		
-		DAOFactory.getFactory().getPreferenciasDAO().update(preferenciasMiMascota);
+		if (preferenciasMiMascota != null) {
+			DAOFactory.getFactory().getPreferenciasDAO().update(preferenciasMiMascota);
+		}
+		else {
+			DAOFactory.getFactory().getPreferenciasDAO().create(preferenciasMiMascota);
+		}
+		
+		
 	}
 
 }
